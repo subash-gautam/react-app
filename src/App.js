ca05ed7a93ac98623bb3273582c002e1674a405c
Subash Gautam <subashgautam977@gmail.com>
2024-05-09 15:23:23 +0545

    try whole web to make toggle dark mode

--- src/App.js
+++ src/App.js
@@ -6,9 +6,18 @@ import About from "./components/About";
 function App() {
     return (
         <>
-            <Navbar title="Suwi" nav1="Home" nav2="Constact" nav3="About Us" />
-            <TextForm heading="Enter the text to analyze" />
-            <About />
+            <Navbar
+                title="Suwi"
+                nav1="Home"
+                nav2="Constact"
+                nav3="About Us"
+                colorMode={colorMode}
+            />
+            <TextForm
+                heading="Enter the text to analyze"
+                colorMode={colorMode}
+            />
+            <About colorMode={colorMode} />
         </>
     );
 }
